53aaa6236bb11be9dd61af20a859f32664619038
Ishan-Chanuka <ickodippiliarachchi@students.nsbm.lk>
2020-10-11 23:45:09 +0530

    Update from Ishan

--- scaler &vector sum,product.c
+++ scaler &vector sum,product.c
@@ -4,7 +4,14 @@ int main()
     int U[5],V[5],W[5],sumU=0,sumV=0,sp=0;
  //suraji
    //thilina
-    //Ishan
+    for (i=0;i<5;i++)
+    {​​
+        W[i]=U[i]*V[i];
+        sp=sp+W[i];
+    }​​
+
+    printf("Scaler product is %d \n",sp);
+
      //Nishu
 
 }
