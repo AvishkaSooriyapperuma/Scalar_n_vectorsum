1bad006f31af919778038279c9a58a1b89a9dc34
Hansi Gayathma <64369844+HansiGaya99@users.noreply.github.com>
2020-10-13 15:33:21 +0700

    Update scaler &vector sum,product.c

--- scaler &vector sum,product.c
+++ scaler &vector sum,product.c
@@ -1,63 +1,59 @@
 #include<stdio.h>
 int main()
 {
-    int U[5],V[5],W[5],sumU=0,sumV=0,sp=0;
+    int U[3],V[3],W[3],SumU=0,SumV=0,Sp=0;
  //suraji
       int i;
 
-     for (i=0;i<5;i++)
-
-     {​​
-
-     printf("enter a value into the element %d of array U ",i+1);
-
-     scanf("%d",&U[i+1]);
-
-     printf("enter a value into the element %d of array V ",i+1);
-
-     scanf("%d",&V[i+1]);
-
-     }​​
+     for (i=0;i<3;i++)
+     {
+         printf("Enter a value into the element %d of array U ",i+1);
+         scanf("%d",&U[i+1]);
+         printf("Enter a value into the element %d of array V ",i+1);
+         scanf("%d",&V[i+1]);
+     }
     
-    for (i=0;i<5;i++)
-    { 
-    sumU= sumU+ U[i];
-    sumV= sumV+ V[i];
+    for (i=0;i<3;i++)
+    {
+        SumU= SumU+ U[i];
+        SumV= SumV+ V[i];
     } 
-    printf("The scaler sum of U is %d \n",sumU);
-    printf("The scaler sum of V is %d \n",sumV);
-    for (i=0;i<5;i++)
-    {​​
+    
+    printf("The Scaler sum of U is %d \n",SumU);
+    printf("The Scaler sum of V is %d \n",SumV);
+    
+    for (i=0;i<3;i++)
+    {
         W[i]=U[i]*V[i];
-        sp=sp+W[i];
-    }​​
+        Sp=Sp+W[i];
+    }
 
-    printf("Scaler product is %d \n",sp);
+    printf("Scaler product is %d \n",Sp);
 
     //vector sum // Nishu
 
     printf("vector sum is \n",)
 
-    for (i=0;i<5;i++)
+    for (i=0;i<3;i++)
 
-    {​​
+    {
 
     W[i]=U[i]+V[i];
 
     printf("%d \n",W[i]);
 
-    }​​
+    }
 
     //vector product
 
     printf("Vector product is \n")
 
-    for (i=0;i<5;i++)
+    for (i=0;i<3;i++)
 
-    {​​
+    {
 
 
 
-}​​
+}
 
 }
